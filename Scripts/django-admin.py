7ca752380cdc9ad3bd0434807507d92aa680b7b7
James Franco <jdfranco@unomaha.edu>
2020-11-02 18:41:01 -0600

    First commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\james franco\box\fall 2020\advanced system design\a1p2\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
