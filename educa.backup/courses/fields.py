33df4820ad59011a0645a22e06e6bb078fddc1a4
James Franco <jdfranco@unomaha.edu>
2020-11-03 19:45:10 -0600

    Working app

--- educa.backup/courses/fields.py
+++ educa.backup/courses/fields.py
@@ -1,12 +1,9 @@
 from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
-
-
 class OrderField(models.PositiveIntegerField):
     def __init__(self, for_fields=None, *args, **kwargs):
         self.for_fields = for_fields
         super().__init__(*args, **kwargs)
-
     def pre_save(self, model_instance, add):
         if getattr(model_instance, self.attname) is None:
             # no current value
--- educa.backup/courses/models.py
+++ educa.backup/courses/models.py
@@ -4,18 +4,13 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from .fields import OrderField
 
-
 class Subject(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
-
     class Meta:
         ordering = ['title']
-
     def __str__(self):
         return self.title
-
-
 class Course(models.Model):
     owner = models.ForeignKey(User,
                               related_name='courses_created',
@@ -27,14 +22,10 @@ class Course(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-
     class Meta:
         ordering = ['-created']
-
     def __str__(self):
         return self.title
-
-
 class Module(models.Model):
     course = models.ForeignKey(Course,
                                related_name='modules',
@@ -42,32 +33,27 @@ class Module(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     order = OrderField(blank=True, for_fields=['course'])
-
+    class Meta:
+        ordering = ['order']
     def __str__(self):
         return f'{self.order}. {self.title}'
 
+class Content(models.Model):
     class Meta:
         ordering = ['order']
-
-
-class Content(models.Model):
     module = models.ForeignKey(Module,
                                related_name='contents',
                                on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType,
                                      on_delete=models.CASCADE,
-                                     limit_choices_to={'model__in':(
-                                     'text',
-                                     'video',
-                                     'image',
-                                     'file')})
+                                     limit_choices_to={'model__in': (
+                                         'text',
+                                         'video',
+                                         'image',
+                                         'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
     order = OrderField(blank=True, for_fields=['module'])
-
-    class Meta:
-        ordering = ['order']
-
 
 
 class ItemBase(models.Model):
@@ -77,21 +63,15 @@ class ItemBase(models.Model):
     title = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
     class Meta:
         abstract = True
-
     def __str__(self):
         return self.title
-
 class Text(ItemBase):
     content = models.TextField()
-
 class File(ItemBase):
     file = models.FileField(upload_to='files')
-
 class Image(ItemBase):
        file = models.FileField(upload_to='images')
-
 class Video(ItemBase):
     url = models.URLField()
